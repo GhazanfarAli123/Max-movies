b3aaf1276182506a7cd52cc1d943f7297a832896
GHAZANFAR-ALI\Ghazanfar Ali <ghazanfar.ali@bramerz.com>
2023-10-13 17:24:35 +0500

    remove search movies and seasons features from nav bar and implenet  it into home page

--- maxmovies-ui/src/Components/navbar.js
+++ maxmovies-ui/src/Components/navbar.js
@@ -1,14 +1,7 @@
 import React ,{useState}from "react"
 
 function navbar() {
-  const [search, setSearch] = useState('')
 
-  const searchBtn = async(e) =>{
-    e.preventDefault()
-     const url = `/search/${search}`;
-     console.log(url)
-     window.open(url,'_self')
-  }
   return (
     <>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -35,13 +28,6 @@ function navbar() {
                 </ul>
               </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <button class="btn btn-outline-success my-2 my-sm-0" onClick={searchBtn} type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
--- maxmovies-ui/src/Pages/HomePage.js
+++ maxmovies-ui/src/Pages/HomePage.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Components/navbar.js'
 
 const HomePage = () => {
 
-    // const handelContextMenu =(e) =>{
-    //   e.preventDefault()
-    // }
+  // const handelContextMenu =(e) =>{
+  //   e.preventDefault()
+  // }
 
-    // document.addEventListener("contextmenu",handelContextMenu)
-    // return()=>{
-    //   document.removeEventListener("contextmenu",handelContextMenu)
-    // }
-    
+  // document.addEventListener("contextmenu",handelContextMenu)
+  // return()=>{
+  //   document.removeEventListener("contextmenu",handelContextMenu)
+  // }
+  const [search, setSearch] = useState('')
+
+  const searchBtn = async (e) => {
+    e.preventDefault()
+    const url = `/search/${search}`;
+    console.log(url)
+    window.open(url, '_self')
+  }
   return (
     <>
-    <Navbar />
-    <div className='search'>
-      <div className='search-screen'>
-        <input className='search-bar' type='text'/>Search
+      <Navbar />
+      <div className='search'>
+        <div className='row'>
+          <div className='col-3'></div>
+          <div className='col-6'>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button class="btn btn-outline-success my-2 my-sm-0" onClick={searchBtn} type="submit">Search</button>
+            </form>
+          </div>
+          <div className='col-3'></div>
+        </div>
       </div>
-    </div>
     </>
   )
 }
