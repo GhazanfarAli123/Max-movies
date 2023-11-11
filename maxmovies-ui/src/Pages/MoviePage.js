f029f4c7d814a62d44494dadc26ec2a0e5715b45
Mian Ali <>
2023-11-11 14:12:29 +0500

    chnagea

---
+++ maxmovies-ui/src/Pages/MoviePage.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import ClientNavBar from '../Components/ClientNavBar';
+import Pr from "../Components/pr"
+
+const MoviePage = () => {
+    const { slug } = useParams();
+    const {Movdata , setMovData} = useState([])
+
+    const getMovSeason = async() =>{
+        try{
+            const {data} = await axios.get(`http://localhost:1000/api/v1/seamov/get-seaMov/${slug}`)
+            // setMovData(data)
+            console.log(data)
+
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{
+        getMovSeason()
+    },[])
+
+    return (
+        <>
+            <ClientNavBar />
+            
+            <div className='container'>
+                <div className='row'>
+                   <div className='col-lg-12'>
+                    
+                   </div>
+                </div>
+            </div>
+           
+        </>
+    );
+}
+
+export default MoviePage;
